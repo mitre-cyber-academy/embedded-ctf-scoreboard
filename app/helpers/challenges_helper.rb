7460ef0bf33444eb54acd871b301f5f9c300d7f9
Robert Clark <rbclark@mitre.org>
2016-08-04 18:28:32 -0400

    Removed update_defensive_points call to delayed job in code.

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -7,9 +7,6 @@ module ChallengesHelper
   # updates the challenge with share amount and point number based on time events
   # also returns the point_value to print in the HTML table
   def update_challenge(challenge)
-    if !Delayed::Job.any? #if there is already a job in there running this, no need to do it again!
-      challenge.update_defensive_points # check if the defensive points need updated, and if so, do it!
-    end
     if !challenge.solved?
       created_at = challenge.created_at.to_datetime
       current_time = DateTime.current
