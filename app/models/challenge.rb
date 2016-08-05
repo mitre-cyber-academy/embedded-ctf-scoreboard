060f255b2f6218822a9bb70fb2943301bae4cf6b
Robert Clark <rbclark@mitre.org>
2016-08-05 15:38:46 -0400

    Merge awarded points back into a single scoreadjustment and fix elapsed time drift bug.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -73,27 +73,28 @@ class Challenge < ActiveRecord::Base
 
   # adds the defensive points to the player if they have held it uncaptured for long enough
   def self.update_defensive_points
-    all.includes(:player).each do |challenge|
+    Player.all.includes(:challenges).each do |player|
       point_value = 0
-      if !challenge.solved?
-        created_at = challenge.created_at.to_datetime
-        current_time = DateTime.current
-        last_updated = created_at #initialize with created_at in case we haven't updated points yet
-        if !challenge.defense_updated_at.nil?
-          last_updated = challenge.defense_updated_at.to_datetime
-        end
-        time_passed = ((current_time - last_updated) * 24).to_i # the amount of time passed since either when it was created or last_updated
-        if time_passed >= challenge.defense_elapsed_time # if we haven't updated defensive points more recently than the time increment then do it
-          time_changes_passed = (time_passed / challenge.defense_elapsed_time).to_i # number of elapsed_times's passed
-          if time_changes_passed > 0
-            point_value = challenge.defense_point_increment * time_changes_passed
-            challenge.defense_updated_at = DateTime.current
-            challenge.save
+      player.challenges.each do |challenge|
+        if !challenge.solved?
+          created_at = challenge.created_at.to_datetime
+          last_updated = created_at #initialize with created_at in case we haven't updated points yet
+          if !challenge.defense_updated_at.nil?
+            last_updated = challenge.defense_updated_at.to_datetime
+          end
+          time_passed = ((DateTime.current - last_updated) * 24).to_i # the amount of time passed since either when it was created or last_updated
+          if time_passed >= challenge.defense_elapsed_time # if we haven't updated defensive points more recently than the time increment then do it
+            time_changes_passed = (time_passed / challenge.defense_elapsed_time).to_i # number of elapsed_times's passed
+            if time_changes_passed > 0
+              point_value += challenge.defense_point_increment * time_changes_passed
+              challenge.defense_updated_at += (time_changes_passed * challenge.defense_elapsed_time.hours)
+              challenge.save
+            end
           end
         end
       end
       if point_value > 0
-        ScoreAdjustment.create!(player: challenge.player, point_value: point_value, text: 'for defending flags!')
+        ScoreAdjustment.create!(player: player, point_value: point_value, text: 'for defending flags!')
       end
     end
   end
